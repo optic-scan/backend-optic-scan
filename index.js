ae9d76d4f7f2feb946c4cab0867508f0c0d84738
Dias <78405123+diasdigital@users.noreply.github.com>
2025-03-16 14:32:35 +0700

    Mengatur cors agar hanya bisa diakses dari url tertentu

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const db = require('./src/configs/database.js');
 const cookieParser = require('cookie-parser');
 // const verifyJWT = require('./src/middleware/verifyJWT.js');
 
-app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
