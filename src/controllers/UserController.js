5b4cb234f5659f22d7b84a4207db242b08a86b75
Dias <78405123+diasdigital@users.noreply.github.com>
2025-03-27 18:00:20 +0700

    Menambah API ubah password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -16,6 +16,28 @@ const getUserProfile = async (req, res) => {
     }
 };
 
+const changePassword = async (req, res) => {
+    const user_id = req.user_id;
+    const { oldPassword, newPassword } = req.body;
+    try {
+        const userFound = await User.findByPk(user_id);
+        if (!userFound) {
+            return res.status(401).json({ message: 'User tidak ditemukan' });
+        }
+        const verifikasi = await bcrypt.compare(
+            oldPassword,
+            userFound.password
+        );
+        if (!verifikasi) {
+            return res.status(401).json({ message: 'Password lama salah' });
+        }
+
+        await User.update({ password: newPassword }, { where: { user_id } });
+        res.json({ message: 'Password berhasil diubah' });
+    } catch (error) {}
+};
+
 module.exports = {
     getUserProfile,
+    changePassword,
 };
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUserProfile } = require('../controllers/UserController.js');
+const {
+    getUserProfile,
+    changePassword,
+} = require('../controllers/UserController.js');
 
 router.get('/profile', getUserProfile);
+router.get('/change-password', changePassword);
 
 module.exports = router;
