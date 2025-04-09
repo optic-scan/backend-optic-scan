0fcaeecb30485afcb3e3b53edcef84f42dcce1e7
Dias <78405123+diasdigital@users.noreply.github.com>
2025-04-09 13:45:34 +0700

    Create RefreshTokenController.js

---
+++ src/controllers/auth/RefreshTokenController.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User.js');
+require('dotenv').config();
+
+const handleRefreshToken = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt)
+        return res.status(401).json({ message: 'Token refresh tidak ada' });
+
+    const refreshToken = cookies.jwt;
+
+    try {
+        const userFound = await User.findOne({
+            where: { refresh_token: refreshToken },
+        });
+
+        if (!userFound)
+            return res.status(403).json({ message: 'Akses ditolak' });
+
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            (err, decoded) => {
+                if (err || decoded.user_id !== userFound.user_id)
+                    return res
+                        .status(403)
+                        .json({ message: 'Token tidak valid' });
+
+                const accessToken = jwt.sign(
+                    { user_id: decoded.user_id, role: decoded.role },
+                    process.env.ACCESS_TOKEN_SECRET,
+                    { expiresIn: '60m' }
+                );
+
+                res.json({ accessToken });
+            }
+        );
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Gagal memperbarui token' });
+    }
+};
+
+module.exports = handleRefreshToken;
