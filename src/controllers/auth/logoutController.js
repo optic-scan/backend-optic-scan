1877fcef7b22a45000acfafbcf543ab8aee1fa84
Dias <78405123+diasdigital@users.noreply.github.com>
2025-04-09 13:44:19 +0700

    Update logoutController.js
    Menambahkan try catch ketika find user

--- src/controllers/auth/logoutController.js
+++ src/controllers/auth/logoutController.js
@@ -1,37 +1,39 @@
 const User = require('../../models/User.js');
 
 const handleLogout = async (req, res) => {
-    const cookies = req.cookies;
-    if (!cookies?.jwt) {
+    const refreshToken = req.cookies?.jwt;
+    if (!refreshToken) {
         return res
             .status(200)
             .json({ message: 'Tidak ada token pada cookie, sudah logout' });
     }
 
-    const refreshToken = cookies.jwt;
+    try {
+        const userFound = await User.findOne({
+            where: { refresh_token: refreshToken },
+        });
+        if (!userFound) {
+            res.clearCookie('jwt', {
+                httpOnly: true,
+                maxAge: 24 * 60 * 60 * 1000,
+                sameSite: 'Lax',
+                secure: false,
+            });
+            return res.status(200).json({
+                message: 'User tidak ditemukan, cookie sudah dibersihkan',
+            });
+        }
 
-    const userFound = await User.findOne({
-        where: { refresh_token: refreshToken },
-    });
-    if (!userFound) {
+        await userFound.update({ refresh_token: null });
         res.clearCookie('jwt', refreshToken, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
         });
-        return res.status(200).json({
-            message: 'User tidak ditemukan, cookie sudah dibersihkan',
-        });
+        return res.status(200).json({ message: 'Logout berhasil' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Gagal logout' });
     }
-
-    await userFound.update(
-        { refresh_token: null },
-        { where: { refresh_token: refreshToken } }
-    );
-    res.clearCookie('jwt', refreshToken, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000,
-    });
-    return res.status(200).json({ message: 'Logout berhasil' });
 };
 
 module.exports = handleLogout;
