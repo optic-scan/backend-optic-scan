419f256ceb5c5bee3ba229b3925f86c5c308aea4
Dias <78405123+diasdigital@users.noreply.github.com>
2025-04-09 13:45:48 +0700

    Update AuthRoutes.js
    Menambah route untuk mendapatkan accessToken yang baru

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -5,10 +5,12 @@ const registerController = require('../controllers/auth/RegisterController.js');
 const loginController = require('../controllers/auth/LoginController.js');
 const logoutController = require('../controllers/auth/LogoutController.js');
 const authenticatedUserController = require('../controllers/auth/AuthenticateUserController.js');
+const handleRefreshToken = require('../controllers/auth/RefreshTokenController');
 
 router.post('/register', registerController);
 router.post('/login', loginController);
 router.get('/logout', logoutController);
 router.get('/auth/me', authenticatedUserController);
+router.get('/auth/refresh', handleRefreshToken);
 
 module.exports = router;
