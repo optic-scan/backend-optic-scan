8b55e620376cb1720669af380833a3b5f5f58e96
Dias <78405123+diasdigital@users.noreply.github.com>
2025-05-07 16:20:53 +0700

    Ubah allow cors ke *

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const verifyJWT = require('./src/middleware/verifyJWT.js');
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: '*',
         credentials: true,
     })
 );
